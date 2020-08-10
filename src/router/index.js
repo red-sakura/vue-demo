82797935d5e7ed263e5dc22aff6d33fc50145250
somliy <1187525390@qq.com>
2020-08-10 09:37:58 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
+    {
+      path: '/',
+      name: 'test',
+      component: () => import('@/views/modules/test/test')
+    }
+  ]
+})
---
+++ src/views/modules/test/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img src="@/assets/logo.png">
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test'
+}
+</script>
+
+<style scoped>
+
+</style>
