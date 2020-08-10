d795b43582ae634e4abefa37cbdb8fea04f4f8b5
mukun <hatunemukun@gmail.com>
2020-08-10 19:45:27 +0800

    8.10练习 追加的文件

---
+++ src/views/modules/demo/counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>hi</h1>
+    <button v-on:click="clickfun">{{title}} You clicked me {{ count }} times.</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'counter',
+  props: ['title'],
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    clickfun: function () {
+      this.count++
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
